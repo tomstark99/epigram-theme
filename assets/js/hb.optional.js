f3951d0ae107a0cae8bd19198c00c497db22b200
tomstark99 <jb18789@bristol.ac.uk>
2021-03-25 00:44:23 +0000

    Time ago update
    - Most posts now show how long it has been since it was posted from:
    -- Just now
    -- mins ago
    -- days ago
    until 6 days
    - It then results back to the default time format after that

--- assets/js/hb.optional.js
+++ assets/js/hb.optional.js
@@ -1,7 +1,27 @@
 Handlebars.registerHelper('if_eq', function(a, b, opts) {
+    alert(a);
+    alert(b);
     if (a == b) {
         return opts.fn(this);
     } else {
         return opts.inverse(this);
     }
 });
+
+Handlebars.registerHelper('announce', function(value) {
+    alert(value);
+    return true;
+});
+
+Handlebars.registerHelper('isdefined', function (value) {
+    return value !== undefined;
+  });
+
+Handlebars.registerHelper("logx", function(something) {
+    console.log(something);
+  });
+
+
+function loggg(val) {
+    console.log(val);
+}
--- assets/js/time.since.js
+++ assets/js/time.since.js
@@ -1,4 +1,5 @@
-function time_ago(time) {
+function time_ago_leg(time) {
+  var time_ = '<i class="fa fa-clock-o"></i><div style="display: inline; margin-left:8px;">';
 
   switch (typeof time) {
     case 'number':
@@ -12,6 +13,7 @@ function time_ago(time) {
     default:
       time = +new Date();
   }
+
   var time_formats = [
     [60, 'seconds', 1], // 60
     [120, '1 minute ago', '1 minute from now'], // 60*2
@@ -33,6 +35,7 @@ function time_ago(time) {
     token = 'ago',
     list_choice = 1;
 
+
   if (seconds == 0) {
     return 'Just now'
   }
@@ -44,12 +47,68 @@ function time_ago(time) {
   var i = 0,
     format;
   while (format = time_formats[i++])
-    if (seconds < format[0]) {
-      if (typeof format[2] == 'string')
-        return format[list_choice];
-      else
-        return Math.floor(seconds / format[2]) + ' ' + format[1] + ' ' + token;
+  if (seconds < format[0]) {
+      if (typeof format[2] == 'string') {
+          var time__ = format[list_choice];
+          window.addEventListener('load', function(){
+            document.getElementById("post_time").innerHTML = time_ + time__;
+            // console.log(time__);
+        })
+        return time__;
     }
+    else {
+        var time__ = Math.floor(seconds / format[2]) + ' ' + format[1] + ' ' + token;
+        window.addEventListener('load', function(){
+            document.getElementById("post_time").innerHTML = time_ + time__;
+            // console.log(time__);
+          })
+          return time__;
+      }
+    }
+    
   return time;
+}
+
+function time_ago(time, uuid) {
+
+    console.log(time);
+    console.log(uuid);
+    var time_ = '<i class="fa fa-clock-o"></i><div style="display: inline; margin-left:4px;">';
+    var time__ = '';
+
+    var posted = new Date(time);
+    var now = new Date();
+
+    var minute = 60.0;
+    var hour = 60.0 * minute;
+    var day = 24.0 * hour;
+    var week = 7 * day;
+
+    var seconds = (now.getTime() - posted.getTime())/1000;
+
+    var time_text = document.getElementById("post_time");
+
+    if ((seconds - (minute * 2.0)) < 0) {
+        time__ = 'Just Now';
+    } else if ((seconds - hour) < 0) {
+        time__ = Math.floor(seconds/60) + ' minutes ago';
+    } else if ((seconds - (hour * 2.0)) < 0) {
+        time__ = Math.floor(seconds/hour) + ' hour ago';
+    } else if ((seconds - day) < 0) {
+        time__ = Math.floor(seconds/hour) + ' hours ago';
+    } else if ((seconds - (day * 2.0)) < 0) {
+        time__ = Math.floor(seconds/day) + ' day ago';
+    } else if ((seconds - week) < 0) {
+        time__ = Math.floor(seconds/day) + ' days ago';
+    } else {
+        var format = { year: 'numeric', month: 'short', day: 'numeric' };
+        time__ = posted.toLocaleDateString("en", format);
+    }
+
+    window.addEventListener('load', function(){
+        document.getElementById(uuid).innerHTML = time_ + '' + time__;
+        // document.getElementById(uuid).removeAttribute('id');
+    })
+    return;
 }
 
