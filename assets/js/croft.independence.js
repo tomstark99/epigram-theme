421ddf567e78471667a32444d1aa5226874e1ffc
Omar Suleiman <jb18940@bristol.ac.uk>
2021-03-26 14:51:16 +0100

    changed to correct croft colour
    - made a croft colour const and changed it on the hover script

--- assets/js/croft.independence.js
+++ assets/js/croft.independence.js
@@ -1,6 +1,8 @@
 var onJOB = false;
 var in_drop = false;
 
+const croft_colour = "#7B52C2";
+
 // if(document.body.classList.contains("tag-the-croft")){
 //     document.getElementById("style").href = "/assets/css/style-croft.css?v=336cc0b901";
 //     document.getElementById("colour").href = "/assets/css/croft-color.css?v=336cc0b901";
@@ -26,19 +28,19 @@ if(!onJOB) {
         if(!evt.relatedTarget.classList.contains("croft_drop")) {
             var elements = document.getElementsByClassName("menu-wrapper");
             for (var i = 0; i < elements.length; i++) {
-                // elements[i].style.backgroundColor = "#CF71EE";
-                elements[i].animate([{backgroundColor:"#c2262f"},{backgroundColor:"#CF71EE"}],{duration:500});
-                elements[i].style.backgroundColor = "#CF71EE";
+                // elements[i].style.backgroundColor = croft_colour;
+                elements[i].animate([{backgroundColor:"#c2262f"},{backgroundColor:croft_colour}],{duration:500});
+                elements[i].style.backgroundColor = croft_colour;
             }
-            croft_drop_id.animate([{backgroundColor:"#c2262f"},{backgroundColor:"#CF71EE"}],{duration:500});
-            croft_drop_id.style.backgroundColor = "#CF71EE";
+            croft_drop_id.animate([{backgroundColor:"#c2262f"},{backgroundColor:croft_colour}],{duration:500});
+            croft_drop_id.style.backgroundColor = croft_colour;
         }
     });
     croft.addEventListener("mouseout", function(evt) {
         if(!evt.relatedTarget.classList.contains("croft_drop")) {
             var elements = document.getElementsByClassName("menu-wrapper");
             for (var i = 0; i < elements.length; i++) {
-                elements[i].animate([{backgroundColor:"#CF71EE"},{backgroundColor:"#c2262f"}],{duration:500});
+                elements[i].animate([{backgroundColor:croft_colour},{backgroundColor:"#c2262f"}],{duration:500});
                 elements[i].style.backgroundColor = "#c2262f";
             }
         }
@@ -55,7 +57,7 @@ for (var i = 0; i < croft_drop.length; i++) {
         if(evt.relatedTarget.id !== "croft_head" && !evt.relatedTarget.classList.contains("croft_drop")) {
             var elements = document.getElementsByClassName("menu-wrapper");
             for (var i = 0; i < elements.length; i++) {
-                elements[i].animate([{backgroundColor:"#CF71EE"},{backgroundColor:"#c2262f"}],{duration:500});
+                elements[i].animate([{backgroundColor:croft_colour},{backgroundColor:"#c2262f"}],{duration:500});
                 elements[i].style.backgroundColor = "#c2262f";
             }
         }
