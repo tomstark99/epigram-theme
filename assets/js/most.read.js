53ebc9a6265f34f98d351d7a87fe82247b85f2d1
Tom Stark <tom.stark.2018@bristol.ac.uk>
2021-03-30 11:51:39 +0100

    More date padding fixed
    - More padding fixes around image cover
    - Added flooring to / week division

--- assets/js/most.read.js
+++ assets/js/most.read.js
@@ -83,7 +83,7 @@ function make_read_request() {
                 <article class="post">
                     <h4 class="title-side"><a href=${post.url}>${post.title}</a></h4>
                     <div class="clear"></div>
-                    <time class="uppercase" datetime=${post.published_at} format='YYYY-MM-DD'><i class="fa fa-clock-o"></i> ${moment(post.published_at).format("MMM DD, YYYY")} <div style="margin-left:8px;display:none;"><i class="fa fa-eye"></i> ${views[i]/3} / week</time>
+                    <time class="uppercase" datetime=${post.published_at} format='YYYY-MM-DD'><i class="fa fa-clock-o"></i> ${moment(post.published_at).format("MMM DD, YYYY")} <div style="margin-left:8px;display:none;"><i class="fa fa-eye"></i> ${Math.floor(views[i]/3)} / week</time>
                 </article>	
                 `
             });
