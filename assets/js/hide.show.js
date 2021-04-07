6490731a1e7a3b165f35dfcfc57d921cbd0041ff
Tom Stark <tom.stark.2018@bristol.ac.uk>
2021-04-07 18:24:30 +0100

    fixing something wrong

--- assets/js/hide.show.js
+++ assets/js/hide.show.js
@@ -7,10 +7,16 @@ function hide_show() {
         if(document.body.classList.contains("tag-the-croft")){
             b.style.background = "#f9e6ff"
             b.style.color = "#B591F2"
+        } else {
+            b.style.background = "#f9e6ff"
+            b.style.color = "#B591F2"
         }
     } else {
         x.style.display = "none";
         if(document.body.classList.contains("tag-the-croft")){
+            b.style.background = "#B591F2"
+            b.style.color = "#fff"
+        } else {
             b.style.background = "#B591F2"
             b.style.color = "#fff"
         }
