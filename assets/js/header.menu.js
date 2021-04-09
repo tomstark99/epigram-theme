da71fcba885ed0f88185281f97b2c13065a0486a
Tom Stark <tom.stark.2018@bristol.ac.uk>
2021-04-09 15:58:15 +0100

    header shows correct nav item
    - fixed the issue where current menu item would not change from home
    - does not work for sub listed items yet

--- assets/js/header.menu.js
+++ assets/js/header.menu.js
@@ -2,24 +2,46 @@ var class_list = document.body.classList;
 var re_croft = new RegExp("^tag-(the-croft|wellbeing|food|travel|style){1}$");
 var re_culture = new RegExp("^tag-(entertainment|film-tv|arts|music){1}$");
 var re_sport = new RegExp("^tag-(sport|intramural|football|basketball|netball|badminton|hockey){1}$");
+var list_items = document.getElementsByTagName("li");
+var nav_items = []
+
+for(var i = 0; i < list_items.length; i++) {
+    if(list_items[i].className.match(/^nav-.*$/)){
+        nav_items.push(list_items[i]);
+    }
+}
 if(class_list.contains("home-template")) {
-    console.log("home");
+    togglehome("HOME");
 } else if (class_list.contains("tag-news")) {
-    console.log("news");
+    togglehome("NEWS");
 } else if (class_list.contains("tag-features")) {
-    console.log("features");
+    togglehome("FEATURES");
 } else if (class_list.contains("tag-comment")) {
-    console.log("opinion");
+    togglehome("OPINION");
 } else if (class_list.contains("tag-science")) {
-    console.log("science");
+    togglehome("SCITECH");
 } else if (class_list.contains("tag-the-croft")) {
-    console.log("the croft");
+    togglehome("THE CROFT");
 } else if (class_list.contains("tag-entertainment")) {
-    console.log("culture");
+    togglehome("CULTURE");
 } else if (class_list.contains("tag-sport")) {
-    console.log("sport");
+    togglehome("SPORT");
 } else if (class_list.contains("tag-puzzles")) {
-    console.log("puzzle");
+    togglehome("PUZZLE");
 } else if (class_list.contains("page-get-involved")) {
-    console.log("get-involved");
+    togglehome("GET INVOLVED");
+}
+
+function togglehome(xd) {
+    for(var i = 0; i < nav_items.length; i++) {
+        if(nav_items[i].innerText === xd && nav_items[i].classList.contains("current-menu-item")) {
+            return;
+        } else {
+            console.log(nav_items[i].innerText, "REMOVED OMG");
+            nav_items[i].classList.remove("current-menu-item");
+            if(nav_items[i].innerText === xd) {
+                nav_items[i].classList.add("current-menu-item");
+            }
+        }
+    }
 }
