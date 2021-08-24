e891f52f857053829a3993b794e6c16790dc4ec7
tomstark99 <jb18789@bristol.ac.uk>
2021-08-24 17:25:56 +0100

    added dynamic ad loading from ghost control panel

---
+++ assets/js/dynamic.images.js
@@ -0,0 +1,25 @@
+function get_top_ad_url() {
+  // get the images page from epigram
+  api.pages.browse({
+    filter: "slug:[images-for-ads-on-epigram]"
+  }).then((pages) => {
+    // creates figure element for each advert
+    var topBanner = document.createElement('figure')
+    var sidebar = document.createElement('figure')
+    // finds figures in page to array
+    var adverts = [...pages[0].html.matchAll(/<figure.*?>(.*?)<\/figure>/g)]
+
+    // first element is top banner
+    topBanner.innerHTML = adverts[0][1]
+    // second element is sidebar
+    sidebar.innerHTML = adverts[1][1]
+
+    // update href and img src
+    document.getElementById("top-banner-sup").href = topBanner.children[1].innerHTML
+    document.getElementById("top-banner-sup").firstChild.src = topBanner.children[0].src
+    document.getElementById("sidebar-sup").href = sidebar.children[1].innerHTML
+    document.getElementById("sidebar-sup").firstChild.src = sidebar.children[0].src
+  })
+}
+// triggers advert images to be loaded from https://epigram.org.uk/images-for-ads-on-epigram/
+get_top_ad_url()
